44dd5b22840da5fdae19d495a9843d8f71bb1dd4
Brad Bieselin <bradley.bieselin@gmail.com>
2022-05-25 20:00:05 -0400

    commit tree 74fa7b74d1930e4db5bb3b8ad50b356663607859

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,23 @@
 class Cohort < ActiveRecord::Base
-  
+  has_many :students
+
+  def add_student(name, age)
+    self.students.create(name: name, age: age, cohort_id: self.id)
+  end
+
+  def average_age
+    self.students.average(:age).to_f
+  end
+
+  def total_students
+    students.calculate(:count, :all)
+  end
+
+  def self.biggest
+    self.all {|cohort_obj| cohort_obj.students.length}.max
+  end
+
+  def self.sort_by_mod
+    self.order(:current_mod)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,34 @@ Student.destroy_all
 # This will create 5 cohorts using a random date
 puts "Seeding cohorts..."
 c1 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-January",
   current_mod: rand(1..5)
 )
 c2 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-February",
   current_mod: rand(1..5)
 )
 c3 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-March",
   current_mod: rand(1..5)
 )
 c4 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-April",
   current_mod: rand(1..5)
 )
 c5 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-May",
   current_mod: rand(1..5)
 )
 
 puts "Seeding students..."
 # TODO: create seed data for students
 # check your schema and make sure to create students with all the necessary data
+s1 = Student.create(name: "Rob", age: 28, cohort_id: c1.id)
+s2 = Student.create(name: "Mike", age: 24, cohort_id: c2.id)
+s3 = Student.create(name: "John", age: 26, cohort_id: c3.id)
+s4 = Student.create(name: "Sarah", age: 25, cohort_id: c4.id)
+s5 = Student.create(name: "Joe", age: 29, cohort_id: c4.id)
+s6 = Student.create(name: "Brad", age: 26, cohort_id: c5.id)
 
 puts "Done!"
